f579b07965488ec6bb598bcd3128c2b85e7b5e44
Kriba95 <kribadiba@gmail.com>
2021-11-10 13:43:13 +0200

    main

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const database = require('./config/database')
+const PORT = process.env.PORT || 3003
+
+//
+
+database.connect((err) => {
+    if (err) throw err;
+}); 
+
+app.use(cors()); //<-- CROSS ORIGIN
+
+
+// EXPRESS TO JSON DATA
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+
+app.get('/api/data', (req, res) => {
+  let sql = 'SELECT * FROM spdata';
+  
+  database.query(sql, (err, result) => {
+      if (err) {
+        res.status(400).send(err);
+        return;
+      }
+      if (result.length) res.json(result);
+      else res.json({});
+  });
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
